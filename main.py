23026b54ccf3aeceb0cc91c5b0b1b52bf72f0660
rrezxth <rienz.edu@icloud.com>
2023-12-19 00:40:58 -0500

    Added new class PayGoldBundle

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ CREDIT_VALUE = 175
 GOLD_VALUE = 140
 
 
-class Bundle:
+class PayMoneyBundle:
     def __init__(self, price, token, credit, gold):
         self.price = price
         self.token = token
@@ -20,8 +20,6 @@ class Bundle:
         if self.gold > 0:
             self.value += self.gold / GOLD_VALUE
 
-        return str(round(self.value, 2))
-
     def get_value(self):
         return str(round(self.value, 2))
 
@@ -30,14 +28,44 @@ class Bundle:
         return str(round(percentage, 2))
 
 
+class PayGoldBundle:
+    def __init__(self, price, token, credit):
+        self.price = price
+        self.token = token
+        self.credit = credit
+        self.value = 0
+
+    def calculate_value(self):
+        if self.token > 0:
+            self.value += self.token / TOKEN_VALUE
+        if self.credit > 0:
+            self.value += self.credit / CREDIT_VALUE
+
+    def get_value(self):
+        return str(round(self.value, 2))
+
+    def percentage_value(self):
+        gold_value = self.price / GOLD_VALUE
+        token_value = self.token / TOKEN_VALUE
+        credit_value = self.credit / CREDIT_VALUE
+
+        currencies_value = token_value + credit_value
+
+        percentage = (currencies_value / gold_value) * 100
+        return str(round(percentage, 2))
+
+
 if __name__ == '__main__':
     user_input = input("Enter values:\t")
     input_list = user_input.split()
     input_numbers = [int(i) for i in input_list]
 
-    bundle = Bundle(input_numbers[0], input_numbers[1], input_numbers[2], input_numbers[3])
+    # bundle = PayMoneyBundle(input_numbers[0], input_numbers[1], input_numbers[2], input_numbers[3])
+    # bundle.calculate_value()
+    # print(f"Bundle value percentage:    {bundle.percentage_value()}%")
+    # print(f"Bundle is worth:    ${bundle.get_value()}")
+
+    bundle = PayGoldBundle(input_numbers[0], input_numbers[1], input_numbers[2])
     bundle.calculate_value()
     print(f"Bundle value percentage:    {bundle.percentage_value()}%")
     print(f"Bundle is worth:    ${bundle.get_value()}")
-
-
