5c304b7bb0741b3ad584d252529eee8c357d2dff
rrezxth <rienz.edu@icloud.com>
2025-04-30 14:06:26 -0400

    Updated for MarvelSnap Snap Packs patch

--- main.py
+++ main.py
@@ -1,77 +1,75 @@
-# CONSTANTS, value per $1
-TOKEN_VALUE = 42.85714
-CREDIT_VALUE = 75
-GOLD_VALUE = 60
+# CONSTANTS, value per 1 unit
+TOKEN_TO_GOLD = 1.3     # 1 Token = 1.3 Gold
+CREDIT_TO_GOLD = 0.8    # 1 Credit = 0.8 Gold
+MONEY_TO_GOLD = 85      # $1 = 85 Gold
 
 
 class BundleBase:
-    def __init__(self, price, token, credit):
-        self.price = price
+    def __init__(self, cost, token, credit):
+        self.cost = cost
         self.token = token
         self.credit = credit
-        self.value = 0
+        self.gold_value = 0  # The value is in Gold currency
 
     def calculate_value(self):
+        self.gold_value = 0    # Reset
         if self.token > 0:
-            self.value += self.token / TOKEN_VALUE
+            self.gold_value += self.token * TOKEN_TO_GOLD
         if self.credit > 0:
-            self.value += self.credit / CREDIT_VALUE
+            self.gold_value += self.credit * CREDIT_TO_GOLD
 
-    def get_value(self):
-        return str(round(self.value, 2))
+    def calculate_bundle_worth(self):
+        return round(self.gold_value, 2)
 
 
 class PayMoneyBundle(BundleBase):
-    def __init__(self, price, token, credit, gold):
-        super().__init__(price, token, credit)
+    def __init__(self, cost, token, credit, gold):
+        super().__init__(cost, token, credit)
         self.gold = gold
+        self.calculate_value()
 
     def calculate_value(self):
+        self.gold_value = 0  # Reset
         super().calculate_value()
         if self.gold > 0:
-            self.value += self.gold / GOLD_VALUE
+            self.gold_value += self.gold
 
     def percentage_value(self):
-        percentage = (self.value / self.price) * 100
+        percentage = ((self.calculate_bundle_worth() / MONEY_TO_GOLD) / self.cost) * 100
         return str(round(percentage, 2))
 
-
 class PayGoldBundle(BundleBase):
-    def __init__(self, price, token, credit):
-        super().__init__(price, token, credit)
+    def __init__(self, cost, token, credit):
+        super().__init__(cost, token, credit)
+        self.calculate_value()      # Run here during __init__ since no calculate_value() method
 
     def percentage_value(self):
-        gold_value = self.price / GOLD_VALUE
-        currencies_value = self.value
-
-        percentage = (currencies_value / gold_value) * 100
+        percentage = (self.calculate_bundle_worth() / self.cost) * 100
         return str(round(percentage, 2))
 
 
 if __name__ == '__main__':
 
+    # Should prolly be a LOOP and an ELSE here
     selection = input("Pay with:\t")
-    if selection == "d":
-        print("Format: [USD, TOKEN, CREDIT, GOLD]")
+    if selection == "m":    #Pay with money
+        print("Format: [USD_COST, TOKEN, CREDIT, GOLD]")
         user_input = input("Enter values:\t")
         input_list = user_input.split()
         input_numbers = [int(i) for i in input_list]
 
         bundle = PayMoneyBundle(input_numbers[0], input_numbers[1], input_numbers[2], input_numbers[3])
-        bundle.calculate_value()
-        print(f"Bundle value percentage:    {bundle.percentage_value()}%")
-        print(f"Bundle is worth:    ${bundle.get_value()}")
-    else:
-        print("Format: [GOLD, TOKEN, CREDIT]")
+        print(f"\nBundle's GOLD value:\t\t\t{bundle.gold_value:.2f}".rstrip('0').rstrip('.') + f" GOLD -- > You paid ${bundle.cost}")
+        print(f"Bundle TOTAL GOLD Value %:\t\t{bundle.percentage_value()}%")
+    elif selection == "g":
+        print("Format: [GOLD_COST, TOKEN, CREDIT]")
         user_input = input("Enter values:\t")
         input_list = user_input.split()
         input_numbers = [int(i) for i in input_list]
 
         bundle = PayGoldBundle(input_numbers[0], input_numbers[1], input_numbers[2])
-        bundle.calculate_value()
-        print(f"Bundle value percentage:    {bundle.percentage_value()}%")
-        print(f"Gold spent is worth:    ${str(round(input_numbers[0]/140, 2))}")
-        print(f"Bundle is worth:    ${bundle.get_value()}")
+        print(f"\nBundle's GOLD value:\t\t\t{bundle.gold_value:.2f}".rstrip('0').rstrip('.') + f" GOLD -- > You paid {bundle.cost} GOLD")
+        print(f"Bundle TOTAL GOLD Value %:\t\t{bundle.percentage_value()}%")
 
 
 
