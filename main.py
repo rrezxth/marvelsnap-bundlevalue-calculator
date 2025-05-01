00f5f7689665b22c04b168ad84bb01819b1bf6d9
rrezxth <rienz.edu@icloud.com>
2025-05-01 06:08:51 -0400

    Updated for MarvelSnap Snap Packs patch -- new values!

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
-# CONSTANTS, value per 1 unit
-TOKEN_TO_GOLD = 1.3     # 1 Token = 1.3 Gold
-CREDIT_TO_GOLD = 0.8    # 1 Credit = 0.8 Gold
-MONEY_TO_GOLD = 85      # $1 = 85 Gold
-
-
 class BundleBase:
-    def __init__(self, cost, token, credit):
+    # Values per (1) card
+    # Some of these values are approximations -- they are NOT end all be all
+    CREDITS_TO_CARD = 9752
+    TOKENS_TO_CARD = 6000
+    GOLD_TO_CARD = 7792     # Assuming a 1.3 Gold to Token ratio
+    MONEY_TO_CARD = 92      # In USD; assuming 1 $Dollar to Gold ratio
+
+    def __init__(self, cost, credit=0, token=0):
         self.cost = cost
-        self.token = token
         self.credit = credit
-        self.gold_value = 0  # The value is in Gold currency
+        self.token = token
+        self.credit_percentage = 0
+        self.token_percentage = 0
+        self.cost_percentage = 0
+        self.value_percentage = 0
 
-    def calculate_value(self):
-        self.gold_value = 0    # Reset
-        if self.token > 0:
-            self.gold_value += self.token * TOKEN_TO_GOLD
-        if self.credit > 0:
-            self.gold_value += self.credit * CREDIT_TO_GOLD
-
-    def calculate_bundle_worth(self):
-        return round(self.gold_value, 2)
-
+    @property
+    def show_value(self):
+        return self.value_percentage
 
 class PayMoneyBundle(BundleBase):
-    def __init__(self, cost, token, credit, gold):
-        super().__init__(cost, token, credit)
+    def __init__(self, cost, credit, token, gold=0):
+        super().__init__(cost, credit, token)
         self.gold = gold
-        self.calculate_value()
+        self.gold_percentage = 0
+        self.calculate_and_store_value()
 
-    def calculate_value(self):
-        self.gold_value = 0  # Reset
-        super().calculate_value()
-        if self.gold > 0:
-            self.gold_value += self.gold
+    def calculate_and_store_value(self):
+        value_num = 0
 
-    def percentage_value(self):
-        percentage = ((self.calculate_bundle_worth() / MONEY_TO_GOLD) / self.cost) * 100
-        return str(round(percentage, 2))
+        if self.credit:
+            self.credit_percentage = (self.credit / self.CREDITS_TO_CARD) * 100
+            value_num += self.credit_percentage
+        if self.token:
+            self.token_percentage = (self.token / self.TOKENS_TO_CARD) * 100
+            value_num += self.token_percentage
+        if self.gold:
+            self.gold_percentage = (self.gold / self.GOLD_TO_CARD) * 100
+            value_num += self.gold_percentage
+
+        self.cost_percentage = (self.cost / self.MONEY_TO_CARD) * 100
+        self.value_percentage = value_num / self.cost_percentage * 100
 
 class PayGoldBundle(BundleBase):
-    def __init__(self, cost, token, credit):
-        super().__init__(cost, token, credit)
-        self.calculate_value()      # Run here during __init__ since no calculate_value() method
+    def __init__(self, cost, credit, token):
+        super().__init__(cost, credit, token)
+        self.calculate_and_store_value()
 
-    def percentage_value(self):
-        percentage = (self.calculate_bundle_worth() / self.cost) * 100
-        return str(round(percentage, 2))
+    def calculate_and_store_value(self):
+        value_num = 0
+
+        if self.credit:
+            self.credit_percentage = (self.credit / self.CREDITS_TO_CARD) * 100
+            value_num += self.credit_percentage
+        if self.token:
+            self.token_percentage = (self.token / self.TOKENS_TO_CARD) * 100
+            value_num += self.token_percentage
+
+        self.cost_percentage = (self.cost / self.GOLD_TO_CARD) * 100
+        self.value_percentage = value_num / self.cost_percentage * 100
 
 
 if __name__ == '__main__':
@@ -53,23 +66,21 @@ if __name__ == '__main__':
     # Should prolly be a LOOP and an ELSE here
     selection = input("Pay with:\t")
     if selection == "m":    #Pay with money
-        print("Format: [USD_COST, TOKEN, CREDIT, GOLD]")
+        print("Format: [USD_COST, CREDIT, TOKEN, GOLD]")
         user_input = input("Enter values:\t")
         input_list = user_input.split()
         input_numbers = [int(i) for i in input_list]
 
         bundle = PayMoneyBundle(input_numbers[0], input_numbers[1], input_numbers[2], input_numbers[3])
-        print(f"\nBundle's GOLD value:\t\t\t{bundle.gold_value:.2f}".rstrip('0').rstrip('.') + f" GOLD -- > You paid ${bundle.cost}")
-        print(f"Bundle TOTAL GOLD Value %:\t\t{bundle.percentage_value()}%")
+        print(f"Bundle TOTAL Value %:\t\t{bundle.show_value:.2f}%".rstrip('0').rstrip('.'))
     elif selection == "g":
-        print("Format: [GOLD_COST, TOKEN, CREDIT]")
+        print("Format: [GOLD_COST, CREDIT, TOKEN]")
         user_input = input("Enter values:\t")
         input_list = user_input.split()
         input_numbers = [int(i) for i in input_list]
 
         bundle = PayGoldBundle(input_numbers[0], input_numbers[1], input_numbers[2])
-        print(f"\nBundle's GOLD value:\t\t\t{bundle.gold_value:.2f}".rstrip('0').rstrip('.') + f" GOLD -- > You paid {bundle.cost} GOLD")
-        print(f"Bundle TOTAL GOLD Value %:\t\t{bundle.percentage_value()}%")
+        print(f"Bundle TOTAL Value %:\t\t{bundle.show_value:.2f}%".rstrip('0').rstrip('.'))
 
 
 
